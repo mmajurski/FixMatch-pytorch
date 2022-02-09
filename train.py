1c0a14c048d5f87fc509526247afda710c72544b
Michael Majurski <mmajurski@gmail.com>
2022-02-09 10:57:34 -0500

    minor changes

--- train.py
+++ train.py
@@ -5,7 +5,6 @@ import os
 import random
 import shutil
 import time
-from collections import OrderedDict
 
 import numpy as np
 import torch
@@ -13,7 +12,6 @@ import torch.nn.functional as F
 import torch.optim as optim
 from torch.optim.lr_scheduler import LambdaLR
 from torch.utils.data import DataLoader, RandomSampler, SequentialSampler
-from torch.utils.data.distributed import DistributedSampler
 from torch.utils.tensorboard import SummaryWriter
 from tqdm import tqdm
 
@@ -115,13 +113,6 @@ def main():
                         help='path to latest checkpoint (default: none)')
     parser.add_argument('--seed', default=None, type=int,
                         help="random seed")
-    parser.add_argument("--amp", action="store_true",
-                        help="use 16-bit (mixed) precision through NVIDIA apex AMP")
-    parser.add_argument("--opt_level", type=str, default="O1",
-                        help="apex AMP optimization level selected in ['O0', 'O1', 'O2', and 'O3']."
-                        "See details at https://nvidia.github.io/apex/amp.html")
-    parser.add_argument("--local_rank", type=int, default=-1,
-                        help="For distributed training: local_rank")
     parser.add_argument('--no-progress', action='store_true',
                         help="don't use progress bar")
 
@@ -145,39 +136,29 @@ def main():
             sum(p.numel() for p in model.parameters())/1e6))
         return model
 
-    if args.local_rank == -1:
-        device = torch.device('cuda', args.gpu_id)
-        args.world_size = 1
-        args.n_gpu = torch.cuda.device_count()
-    else:
-        torch.cuda.set_device(args.local_rank)
-        device = torch.device('cuda', args.local_rank)
-        torch.distributed.init_process_group(backend='nccl')
-        args.world_size = torch.distributed.get_world_size()
-        args.n_gpu = 1
+    device = torch.device('cuda', args.gpu_id)
+    # args.world_size = 1
+    args.n_gpu = torch.cuda.device_count()
 
     args.device = device
+    # args.num_workers = 0  # TODO enable this for debugging so everything happens on the master thread
 
     logging.basicConfig(
         format="%(asctime)s - %(levelname)s - %(name)s -   %(message)s",
         datefmt="%m/%d/%Y %H:%M:%S",
-        level=logging.INFO if args.local_rank in [-1, 0] else logging.WARN)
+        level=logging.INFO)
 
     logger.warning(
-        f"Process rank: {args.local_rank}, "
         f"device: {args.device}, "
-        f"n_gpu: {args.n_gpu}, "
-        f"distributed training: {bool(args.local_rank != -1)}, "
-        f"16-bits training: {args.amp}",)
+        f"n_gpu: {args.n_gpu}, ")
 
     logger.info(dict(args._get_kwargs()))
 
     if args.seed is not None:
         set_seed(args)
 
-    if args.local_rank in [-1, 0]:
-        os.makedirs(args.out, exist_ok=True)
-        args.writer = SummaryWriter(args.out)
+    os.makedirs(args.out, exist_ok=True)
+    args.writer = SummaryWriter(args.out)
 
     if args.dataset == 'cifar10':
         args.num_classes = 10
@@ -199,16 +180,10 @@ def main():
             args.model_depth = 29
             args.model_width = 64
 
-    if args.local_rank not in [-1, 0]:
-        torch.distributed.barrier()
-
     labeled_dataset, unlabeled_dataset, test_dataset = DATASET_GETTERS[args.dataset](
         args, './data')
 
-    if args.local_rank == 0:
-        torch.distributed.barrier()
-
-    train_sampler = RandomSampler if args.local_rank == -1 else DistributedSampler
+    train_sampler = RandomSampler
 
     labeled_trainloader = DataLoader(
         labeled_dataset,
@@ -230,13 +205,10 @@ def main():
         batch_size=args.batch_size,
         num_workers=args.num_workers)
 
-    if args.local_rank not in [-1, 0]:
-        torch.distributed.barrier()
-
     model = create_model(args)
 
-    if args.local_rank == 0:
-        torch.distributed.barrier()
+    # wrap the model into a single node DataParallel
+    model = torch.nn.DataParallel(model)
 
     model.to(args.device)
 
@@ -258,6 +230,9 @@ def main():
         from models.ema import ModelEMA
         ema_model = ModelEMA(args, model, args.ema_decay)
 
+    # setup AMP
+    scaler = torch.cuda.amp.GradScaler()
+
     args.start_epoch = 0
 
     if args.resume:
@@ -274,42 +249,23 @@ def main():
         optimizer.load_state_dict(checkpoint['optimizer'])
         scheduler.load_state_dict(checkpoint['scheduler'])
 
-    if args.amp:
-        from apex import amp
-        model, optimizer = amp.initialize(
-            model, optimizer, opt_level=args.opt_level)
-
-    if args.local_rank != -1:
-        model = torch.nn.parallel.DistributedDataParallel(
-            model, device_ids=[args.local_rank],
-            output_device=args.local_rank, find_unused_parameters=True)
 
     logger.info("***** Running training *****")
     logger.info(f"  Task = {args.dataset}@{args.num_labeled}")
     logger.info(f"  Num Epochs = {args.epochs}")
     logger.info(f"  Batch size per GPU = {args.batch_size}")
-    logger.info(
-        f"  Total train batch size = {args.batch_size*args.world_size}")
     logger.info(f"  Total optimization steps = {args.total_steps}")
 
     model.zero_grad()
     train(args, labeled_trainloader, unlabeled_trainloader, test_loader,
-          model, optimizer, ema_model, scheduler)
+          model, optimizer, ema_model, scheduler, scaler)
 
 
 def train(args, labeled_trainloader, unlabeled_trainloader, test_loader,
-          model, optimizer, ema_model, scheduler):
-    if args.amp:
-        from apex import amp
+          model, optimizer, ema_model, scheduler, scaler):
     global best_acc
     test_accs = []
     end = time.time()
-
-    if args.world_size > 1:
-        labeled_epoch = 0
-        unlabeled_epoch = 0
-        labeled_trainloader.sampler.set_epoch(labeled_epoch)
-        unlabeled_trainloader.sampler.set_epoch(unlabeled_epoch)
 
     labeled_iter = iter(labeled_trainloader)
     unlabeled_iter = iter(unlabeled_trainloader)
@@ -323,53 +279,81 @@ def train(args, labeled_trainloader, unlabeled_trainloader, test_loader,
         losses_u = AverageMeter()
         mask_probs = AverageMeter()
         if not args.no_progress:
-            p_bar = tqdm(range(args.eval_step),
-                         disable=args.local_rank not in [-1, 0])
+            p_bar = tqdm(range(args.eval_step))
         for batch_idx in range(args.eval_step):
             try:
                 inputs_x, targets_x = labeled_iter.next()
             except:
-                if args.world_size > 1:
-                    labeled_epoch += 1
-                    labeled_trainloader.sampler.set_epoch(labeled_epoch)
                 labeled_iter = iter(labeled_trainloader)
                 inputs_x, targets_x = labeled_iter.next()
 
             try:
                 (inputs_u_w, inputs_u_s), _ = unlabeled_iter.next()
             except:
-                if args.world_size > 1:
-                    unlabeled_epoch += 1
-                    unlabeled_trainloader.sampler.set_epoch(unlabeled_epoch)
                 unlabeled_iter = iter(unlabeled_trainloader)
                 (inputs_u_w, inputs_u_s), _ = unlabeled_iter.next()
 
             data_time.update(time.time() - end)
             batch_size = inputs_x.shape[0]
-            inputs = interleave(
-                torch.cat((inputs_x, inputs_u_w, inputs_u_s)), 2*args.mu+1).to(args.device)
+
+            # place data on device
+            inputs_x = inputs_x.to(args.device)
+            inputs_u_w = inputs_u_w.to(args.device)
+            inputs_u_s = inputs_u_s.to(args.device)
+
             targets_x = targets_x.to(args.device)
-            logits = model(inputs)
-            logits = de_interleave(logits, 2*args.mu+1)
-            logits_x = logits[:batch_size]
-            logits_u_w, logits_u_s = logits[batch_size:].chunk(2)
-            del logits
 
-            Lx = F.cross_entropy(logits_x, targets_x, reduction='mean')
+            use_amp = True
 
-            pseudo_label = torch.softmax(logits_u_w.detach()/args.T, dim=-1)
-            max_probs, targets_u = torch.max(pseudo_label, dim=-1)
-            mask = max_probs.ge(args.threshold).float()
+            if use_amp:
+                with torch.cuda.amp.autocast():
+                    inputs = interleave(torch.cat((inputs_x, inputs_u_w, inputs_u_s)), 2*args.mu+1)
+                    logits = model(inputs)
+                    start_time = time.time()
+                    logits = de_interleave(logits, 2*args.mu+1)
+                    logits_x = logits[:batch_size]
+                    logits_u_w, logits_u_s = logits[batch_size:].chunk(2)
+                    del logits
 
-            Lu = (F.cross_entropy(logits_u_s, targets_u,
-                                  reduction='none') * mask).mean()
+                    Lx = F.cross_entropy(logits_x, targets_x, reduction='mean')
 
-            loss = Lx + args.lambda_u * Lu
+                    pseudo_label = torch.softmax(logits_u_w.detach()/args.T, dim=-1)
+                    max_probs, targets_u = torch.max(pseudo_label, dim=-1)
+                    mask = max_probs.ge(args.threshold).float()
 
-            if args.amp:
-                with amp.scale_loss(loss, optimizer) as scaled_loss:
-                    scaled_loss.backward()
+                    Lu = (F.cross_entropy(logits_u_s, targets_u,
+                                          reduction='none') * mask).mean()
+
+                    loss = Lx + args.lambda_u * Lu
+
+                # Scales loss.  Calls backward() on scaled loss to create scaled gradients.
+                # Backward passes under autocast are not recommended.
+                # Backward ops run in the same dtype autocast chose for corresponding forward ops.
+                scaler.scale(loss).backward()
+                # scaler.step() first unscales the gradients of the optimizer's assigned params.
+                # If these gradients do not contain infs or NaNs, optimizer.step() is then called,
+                # otherwise, optimizer.step() is skipped.
+                scaler.step(optimizer)
+                # Updates the scale for next iteration.
+                scaler.update()
             else:
+                inputs = interleave(torch.cat((inputs_x, inputs_u_w, inputs_u_s)), 2 * args.mu + 1)
+                logits = model(inputs)
+                logits = de_interleave(logits, 2 * args.mu + 1)
+                logits_x = logits[:batch_size]
+                logits_u_w, logits_u_s = logits[batch_size:].chunk(2)
+                del logits
+
+                Lx = F.cross_entropy(logits_x, targets_x, reduction='mean')
+
+                pseudo_label = torch.softmax(logits_u_w.detach() / args.T, dim=-1)
+                max_probs, targets_u = torch.max(pseudo_label, dim=-1)
+                mask = max_probs.ge(args.threshold).float()
+
+                Lu = (F.cross_entropy(logits_u_s, targets_u,
+                                      reduction='none') * mask).mean()
+
+                loss = Lx + args.lambda_u * Lu
                 loss.backward()
 
             losses.update(loss.item())
@@ -407,40 +391,38 @@ def train(args, labeled_trainloader, unlabeled_trainloader, test_loader,
         else:
             test_model = model
 
-        if args.local_rank in [-1, 0]:
-            test_loss, test_acc = test(args, test_loader, test_model, epoch)
+        test_loss, test_acc = test(args, test_loader, test_model, epoch)
 
-            args.writer.add_scalar('train/1.train_loss', losses.avg, epoch)
-            args.writer.add_scalar('train/2.train_loss_x', losses_x.avg, epoch)
-            args.writer.add_scalar('train/3.train_loss_u', losses_u.avg, epoch)
-            args.writer.add_scalar('train/4.mask', mask_probs.avg, epoch)
-            args.writer.add_scalar('test/1.test_acc', test_acc, epoch)
-            args.writer.add_scalar('test/2.test_loss', test_loss, epoch)
+        args.writer.add_scalar('train/1.train_loss', losses.avg, epoch)
+        args.writer.add_scalar('train/2.train_loss_x', losses_x.avg, epoch)
+        args.writer.add_scalar('train/3.train_loss_u', losses_u.avg, epoch)
+        args.writer.add_scalar('train/4.mask', mask_probs.avg, epoch)
+        args.writer.add_scalar('test/1.test_acc', test_acc, epoch)
+        args.writer.add_scalar('test/2.test_loss', test_loss, epoch)
 
-            is_best = test_acc > best_acc
-            best_acc = max(test_acc, best_acc)
+        is_best = test_acc > best_acc
+        best_acc = max(test_acc, best_acc)
 
-            model_to_save = model.module if hasattr(model, "module") else model
-            if args.use_ema:
-                ema_to_save = ema_model.ema.module if hasattr(
-                    ema_model.ema, "module") else ema_model.ema
-            save_checkpoint({
-                'epoch': epoch + 1,
-                'state_dict': model_to_save.state_dict(),
-                'ema_state_dict': ema_to_save.state_dict() if args.use_ema else None,
-                'acc': test_acc,
-                'best_acc': best_acc,
-                'optimizer': optimizer.state_dict(),
-                'scheduler': scheduler.state_dict(),
-            }, is_best, args.out)
+        model_to_save = model.module if hasattr(model, "module") else model
+        if args.use_ema:
+            ema_to_save = ema_model.ema.module if hasattr(
+                ema_model.ema, "module") else ema_model.ema
+        save_checkpoint({
+            'epoch': epoch + 1,
+            'state_dict': model_to_save.state_dict(),
+            'ema_state_dict': ema_to_save.state_dict() if args.use_ema else None,
+            'acc': test_acc,
+            'best_acc': best_acc,
+            'optimizer': optimizer.state_dict(),
+            'scheduler': scheduler.state_dict(),
+        }, is_best, args.out)
 
-            test_accs.append(test_acc)
-            logger.info('Best top-1 acc: {:.2f}'.format(best_acc))
-            logger.info('Mean top-1 acc: {:.2f}\n'.format(
-                np.mean(test_accs[-20:])))
+        test_accs.append(test_acc)
+        logger.info('Best top-1 acc: {:.2f}'.format(best_acc))
+        logger.info('Mean top-1 acc: {:.2f}\n'.format(
+            np.mean(test_accs[-20:])))
 
-    if args.local_rank in [-1, 0]:
-        args.writer.close()
+    args.writer.close()
 
 
 def test(args, test_loader, model, epoch):
@@ -452,8 +434,7 @@ def test(args, test_loader, model, epoch):
     end = time.time()
 
     if not args.no_progress:
-        test_loader = tqdm(test_loader,
-                           disable=args.local_rank not in [-1, 0])
+        test_loader = tqdm(test_loader)
 
     with torch.no_grad():
         for batch_idx, (inputs, targets) in enumerate(test_loader):
